d4251fabfeba40a3816759b3411851ee55e346f6
Salaruddin <salaruddinjalal@gmail.com>
2024-10-23 13:26:32 +0430

    Added Dtos and Static Roles

---
+++ JwtAuthentication_ASP.net8/Core/Dtos/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtAuthentication_ASP.net8.Core.Dtos
+{
+    public record LoginDto
+    {
+        [Required(ErrorMessage ="Username is Required")]
+        string UserName;
+
+        [Required(ErrorMessage ="Password is Required")]
+        string Password;
+
+    }
+}
---
+++ JwtAuthentication_ASP.net8/Core/Dtos/UpdatePermissionDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtAuthentication_ASP.net8.Core.Dtos
+{
+    public class UpdatePermissionDto
+    {
+        [Required(ErrorMessage ="UserName is required")]
+        public string UserName { get; set; }
+    }
+}
