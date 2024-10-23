06b0e39c70900576a78b2f52ea9980d12e0e5b0b
Salaruddin <salaruddinjalal@gmail.com>
2024-10-23 12:56:16 +0430

    implement database context and Identity with Bearer Token

---
+++ JwtAuthentication_ASP.net8/Core/Context/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuthentication_ASP.net8.Core.Context
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
