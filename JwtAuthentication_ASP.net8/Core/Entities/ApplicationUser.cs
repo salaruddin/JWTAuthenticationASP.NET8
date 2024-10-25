7f4847d9b60dc38e26d4d80b16db8692d33f4e29
Salaruddin <salaruddinjalal@gmail.com>
2024-10-25 10:03:26 +0430

    Final Commit of V2, replacing IdentityUser with ApplicationUser

---
+++ JwtAuthentication_ASP.net8/Core/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace JwtAuthentication_ASP.net8.Core.Entities
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ JwtAuthentication_ASP.net8/Migrations/20241025050559_Add-ApplicationUser-Inheriting-From-IdentityUser-FirstNameLastNameAge.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JwtAuthentication_ASP.net8.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApplicationUserInheritingFromIdentityUserFirstNameLastNameAge : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                nullable: false,
+                defaultValue: "User");
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                nullable: false,
+                defaultValue: "User");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+        }
+    }
+}
